9f9748162fa58c513cb6be7c46ebf4a295aacd63
Miles <miles.martin@nyu.edu>
2020-02-20 18:53:15 -0500

    Plotted first point on map

---
+++ js/locations.js
@@ -0,0 +1,7 @@
+var allClassLocations = [
+  {
+    "info": "Silver Center for Arts and Science",
+    "latitude": 40.730473,
+    "longitude": -73.995591,
+  },
+]
--- js/scripts.js
+++ js/scripts.js
@@ -3,66 +3,19 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWlsZXNtaWxlc21pbGVzIiwiYSI6ImNrNms1NGplYzAwb
 var initOptions = {
   container: 'mapdiv',
   style: 'mapbox://styles/mapbox/dark-v10',
-  center: [-73.96, 40.72],
-  zoom: 12
+  center: [-74, 40.73],
+  zoom: 15
 }
 
 var mapObject = new mapboxgl.Map(initOptions)
 
-pizzaDirectory.forEach(
-  function(pizzaSpot) {
+allClassLocations.forEach(
+  function(classLocation) {
     new mapboxgl.Marker()
-      .setLngLat([pizzaSpot.longitude,pizzaSpot.latitude])
+      .setLngLat([classLocation.longitude,classLocation.latitude])
       .setPopup(
         new mapboxgl.Popup({offset: 25})
-        .setHTML(`<p>${pizzaSpot.name} likes ${pizzaSpot.pizza_place}</p>`)
+        .setHTML(`<p>${classLocation.info}</p>`)
       )
       .addTo(mapObject)
 })
-
-
-
-
-// loadGeoJSON
-
-// var geojson = {
-//   type: 'FeatureCollection',
-//   features: [{
-//     type: 'Feature',
-//     geometry: {
-//       type: 'Point',
-//       coordinates: [-77.032, 38.913]
-//     },
-//     properties: {
-//       title: 'Mapbox',
-//       description: 'Washington, D.C.'
-//     }
-//   },
-//   {
-//     type: 'Feature',
-//     geometry: {
-//       type: 'Point',
-//       coordinates: [-122.414, 37.776]
-//     },
-//     properties: {
-//       title: 'Mapbox',
-//       description: 'San Francisco, California'
-//     }
-//   }]
-// };
-
-// add markers to map
-// geojson.features.forEach(function(marker) {
-//
-//   // create a HTML element for each feature
-//   var el = document.createElement('div');
-//   el.className = 'marker';
-//
-//   // make a marker for each feature and add to the map
-//   new mapboxgl.Marker(el)
-//     .setLngLat(marker.geometry.coordinates)
-//   .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-//     .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
-//   .addTo(map);
-//
-// });
