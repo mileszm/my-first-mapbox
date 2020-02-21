a0fe44b3668042c2ad49d81371eb58b03b657ae1
Miles <miles.martin@nyu.edu>
2020-02-20 19:02:47 -0500

    Added second point

--- js/locations.js
+++ js/locations.js
@@ -4,4 +4,9 @@ var allClassLocations = [
     "latitude": 40.730473,
     "longitude": -73.995591,
   },
+  {
+    "info": "Bobst Library",
+    "latitude": 40.7295678,
+    "longitude": -73.9970934,
+  }
 ]
